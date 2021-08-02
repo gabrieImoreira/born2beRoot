22d4948dda292cdbefaffe4a9736144944c01625
gams <gabriel.antonioms@gmail.com>
2021-08-02 17:21:43 -0300

    add final version monitoring

--- monitoring.sh
+++ monitoring.sh
@@ -1,33 +1,20 @@
 #!/bin/bash
-arc=$(uname -a)
-pcpu=$(grep "physical id" /proc/cpuinfo | sort | uniq | wc -l)
-vcpu=$(grep "^processor" /proc/cpuinfo | wc -l)
-fram=$(free -m | awk '$1 == "Mem:" {print $2}')
-uram=$(free -m | awk '$1 == "Mem:" {print $3}')
-pram=$(free | awk '$1 == "Mem:" {printf("%.2f"), $3/$2*100}')
-fdisk=$(df -Bg | grep '^/dev/' | grep -v '/boot$' | awk '{ft += $2} END {print ft}')
-udisk=$(df -Bm | grep '^/dev/' | grep -v '/boot$' | awk '{ut += $3} END {print ut}')
-pdisk=$(df -Bm | grep '^/dev/' | grep -v '/boot$' | awk '{ut += $3} {ft+= $2} END {printf("%d"), ut/ft*100}')
-cpul=$(top -bn1 | grep '^%Cpu' | cut -c 9- | xargs | awk '{printf("%.1f%%"), $1 + $3}')
-lb=$(who -b | awk '$1 == "system" {print $3 " " $4}')
-lvmt=$(lsblk | grep "lvm" | wc -l)
-lvmu=$(if [ $lvmt -eq 0 ]; then echo no; else echo yes; fi)
-#You need to install net tools for the next step [$ sudo apt install net-tools]
-ctcp=$(cat /proc/net/sockstat{,6} | awk '$1 == "TCP:" {print $3}')
-ulog=$(users | wc -w)
-ip=$(hostname -I)
-mac=$(ip link show | awk '$1 == "link/ether" {print $2}')
-cmds=$(journalctl _COMM=sudo | grep COMMAND | wc -l) # journalctl should be running as sudo but our script is running as root so we don't need in sudo here
-wall "	#Architecture: $arc
-	#CPU physical: $pcpu
-	#vCPU: $vcpu
-	#Memory Usage: $uram/${fram}MB ($pram%)
-	#Disk Usage: $udisk/${fdisk}Gb ($pdisk%)
-	#CPU load: $cpul
-	#Last boot: $lb
-	#LVM use: $lvmu
-	#Connexions TCP: $ctcp ESTABLISHED
-	#User log: $ulog
-	#Network: IP $ip ($mac)
-	#Sudo: $cmds cmd" # broadcast our system information on all terminals
+echo "#Architecture :" "$(uname -a)"
+echo "#CPU physical :" "$(grep 'physical id' /proc/cpuinfo | uniq | wc -l)"
+echo "#vCPU         :" "$(grep 'processor' /proc/cpuinfo | uniq | wc -l)"
+USED_R=$(free -m |awk 'NR == 2 {print $3}')
+TOTAL_R=$(free -m |awk 'NR == 2 {print $2}')
+PRCT_R=$(free -m |awk 'NR == 2 {printf "%.2f", $3*100/$2}')
+echo "#Memory Usage :" "${USED_R}/${TOTAL_R}MB" "(${PRCT_R}%)"
+USED_D=$(df -m --total | awk 'END{print $3}')
+TOTAL_D=$(df -h --total | awk 'END{print $2}')
+PRCT_D=$(df -h --total | awk 'END{print $5}')
+echo "#Disk Usage   :" "${USED_D}/${TOTAL_D}b" "(${PRCT_D})"
+echo "#CPU Load     :" "$(top -bn1 | grep '^%Cpu' | cut -c 9- | xargs | awk '{printf("%.1f%%"), $1 + $3}')"
+echo "#Last Boot    :" "$(who -b | awk '{print $3,$4}')"
+echo "#LVM in Use   :" $(if [ $(/usr/sbin/blkid | grep -c '/dev/mapper') -eq 0 ]; then echo "no"; else echo "yes"; fi)
+echo "#Connexions TCP  :" "$(ss -s | awk '/TCP:/ {print $2}') ESTABLISHED"
+echo "#Users Logged :" "$(who | wc -l)"
+echo "#Network      :" "IP $(hostname -I)" "($(/usr/sbin/ifconfig | awk '/ether/ {print $2}'))"
+echo "#Sudo         :" "$(grep -c 'COMMAND' /var/log/sudo/sudo.log)" "cmd"
 
